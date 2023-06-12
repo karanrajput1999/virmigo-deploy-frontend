8874cb0967e6f5b6313e121c4376588bf5055549
Karan <rajputkaran441@gmail.com>
2023-06-13 01:13:17 +0530

    mobile nav completed

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
 import CottageIcon from "@mui/icons-material/Cottage"
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt"
 import NotificationsIcon from "@mui/icons-material/Notifications"
 import AccountCircleIcon from "@mui/icons-material/AccountCircle"
 import MenuIcon from "@mui/icons-material/Menu"
+import CloseIcon from "@mui/icons-material/Close"
 
 const menuIcon = {
   fontSize: "35px",
@@ -10,12 +12,14 @@ const menuIcon = {
 }
 
 function Navbar() {
+  const [navClose, setNavClose] = useState(true)
+
   return (
     <div className="navbar-container">
-      <nav className="flex space-between align-center container">
+      <nav className="flex space-between align-center container" id="navbar">
         <h1 className="logo">Virmigo</h1>
         {/* removed from ul className="flex align-center" */}
-        <ul>
+        <ul className={`${navClose ? "navClose" : ""}`}>
           <li className="flex align-center">
             <CottageIcon style={menuIcon} />
           </li>
@@ -26,11 +30,22 @@ function Navbar() {
             <NotificationsIcon style={menuIcon} />
           </li>
         </ul>
-        <div className="hamburger flex align-center">
-          <MenuIcon style={{ ...menuIcon }} />
-        </div>
-        <div className="flex align-center">
+
+        <div className="user-menu flex align-center">
           <AccountCircleIcon style={menuIcon} />
+        </div>
+        <div
+          className="flex align-center"
+          id="hamburger"
+          onClick={() => {
+            setNavClose(!navClose)
+          }}
+        >
+          {navClose ? (
+            <MenuIcon style={menuIcon} />
+          ) : (
+            <CloseIcon style={menuIcon} />
+          )}
         </div>
       </nav>
     </div>
