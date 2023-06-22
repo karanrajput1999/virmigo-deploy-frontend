3b68af16660ed6cea2448c005633ec8bdbff526d
Karan <rajputkaran441@gmail.com>
2023-06-22 22:34:32 +0530

    image preview while creating post

--- src/Components/PostForm.tsx
+++ src/Components/PostForm.tsx
@@ -1,12 +1,26 @@
-import React from "react"
+import React, { ChangeEvent, useState } from "react"
 import Paper from "@mui/material/Paper"
-import Box from "@mui/material/Box"
 import userIcon from "../assets/user-icon.png"
 import PermMediaIcon from "@mui/icons-material/PermMedia"
 import SendIcon from "@mui/icons-material/Send"
 import GifBoxIcon from "@mui/icons-material/GifBox"
 
 function PostForm() {
+  const [previewImg, setPreviewImg] = useState<string>("")
+
+  function previewPost(e: ChangeEvent<HTMLInputElement>): void {
+    const previewPic = e.target.files?.[0]
+    if (previewPic) {
+      const reader = new FileReader()
+      reader.onload = () => {
+        setPreviewImg(reader.result as string)
+      }
+      reader.readAsDataURL(previewPic)
+    } else {
+      setPreviewImg("")
+    }
+  }
+
   return (
     <div className="flex content-center post-form-container">
       <div style={{ width: "90%", height: "100%" }}>
@@ -23,12 +37,28 @@ function PostForm() {
               placeholder="Share your thoughts..."
             />
           </div>
+
+          {previewImg && (
+            <div className="post-preview">
+              <img src={previewImg} alt="preview image" />
+            </div>
+          )}
           <div className="flex align-center space-between post-action-section">
             <div className="post-media-container">
-              <PermMediaIcon
-                className="media-icon"
-                style={{ color: "#5600ac", cursor: "pointer" }}
+              <label htmlFor="media-icon">
+                <PermMediaIcon
+                  className="media-icon"
+                  name="media-icon"
+                  style={{ color: "#5600ac", cursor: "pointer" }}
+                />
+              </label>
+              <input
+                type="file"
+                id="media-icon"
+                style={{ display: "none" }}
+                onChange={previewPost}
               />
+
               <GifBoxIcon
                 className="media-icon"
                 style={{ color: "#5600ac", cursor: "pointer" }}
