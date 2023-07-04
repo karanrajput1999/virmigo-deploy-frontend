956f298bfa0bca83defefbbcdaa27dc095879c8e
Karan <rajputkaran441@gmail.com>
2023-07-04 22:02:16 +0530

    Used Yup for validation

--- src/Components/Signup.tsx
+++ src/Components/Signup.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { FormikValues, useFormik } from "formik"
+import * as Yup from "yup"
 
 interface formValues {
   name: string
@@ -20,36 +21,20 @@ const onSubmit = (values: formValues) => {
   console.log(values)
 }
 
-const validate = (values: formValues) => {
-  let errors: FormikValues = {}
-  if (!values.name) {
-    errors.name = "Name is required!"
-  }
-  if (!values.email) {
-    errors.email = "Email is required!"
-  } else if (
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-      values.email,
-    )
-  ) {
-    errors.email = "Invalid email format!"
-  }
-
-  if (!values.password) {
-    errors.password = "Password is required!"
-  }
-  if (!values.confirmPassword) {
-    errors.confirmPassword = "Confirm Password is required!"
-  }
-
-  return errors
-}
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is required!"),
+  email: Yup.string()
+    .email("Invalid email format!")
+    .required("Email is required!"),
+  password: Yup.string().required("Password is required!"),
+  confirmPassword: Yup.string().required("Confirm Password is required!"),
+})
 
 function Signup() {
   const formik = useFormik<formValues>({
     initialValues,
     onSubmit,
-    validate,
+    validationSchema,
   })
 
   console.log("Form errors", formik.errors)
@@ -84,7 +69,7 @@ function Signup() {
           <div className="flex flex-column">
             <label htmlFor="email">Email</label>
             <input
-              type="text"
+              type="email"
               id="email"
               name="email"
               placeholder="johndoe@gmail.com"
