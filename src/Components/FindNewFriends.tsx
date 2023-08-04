dc2887dc5933ddef9dee915445731910b6288253
Karan <rajputkaran441@gmail.com>
2023-08-04 21:57:12 +0530

    homepage friend request system working

--- src/Components/FindNewFriends.tsx
+++ src/Components/FindNewFriends.tsx
@@ -23,14 +23,63 @@ interface UserType {
 
 function FindNewFriends() {
   const [allUsers, setAllUsers] = useState<UserType[] | null>(null)
+  const [friendRequestedUser, setFriendRequestedUser] = useState<string[]>([])
+
+  function getFriendRequestedUserId(friendRequestedUser: UserType[]) {
+    const requestedUsers = friendRequestedUser.map((user) => {
+      return user._id
+    })
+    setFriendRequestedUser(requestedUsers)
+  }
 
   useEffect(() => {
     axios
       .get("http://localhost:3000/", { withCredentials: true })
       .then((res) => {
+        console.log("received friend request data", res.data)
+        getFriendRequestedUserId(res.data.allFriendRequestsSent)
         setAllUsers(res.data.allUsers)
       })
   }, [])
+
+  function sendFriendRequest(friendRequestReceiverId: string) {
+    axios
+      .post(
+        "http://localhost:3000/",
+        { friendRequestReceiverId },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        setFriendRequestedUser([
+          ...friendRequestedUser,
+          friendRequestReceiverId,
+        ])
+        console.log("after sending a friend request", res.data)
+      })
+      .catch((error) => {
+        console.log("error while sending friend request", error)
+      })
+  }
+
+  function cancelFriendRequest(receiverId: string) {
+    axios
+      .post(
+        "http://localhost:3000/",
+        { cancelRequestId: receiverId },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        setFriendRequestedUser(
+          friendRequestedUser.filter(
+            (friendRequest) => friendRequest !== receiverId,
+          ),
+        )
+        console.log("after sending a friend request", res.data)
+      })
+      .catch((error) => {
+        console.log("error while sending friend request", error)
+      })
+  }
 
   return (
     <div className="mx find-new-friends-Container">
@@ -61,8 +110,18 @@ function FindNewFriends() {
                     <span className="friends-name">{user.name}</span>
                   </div>
 
-                  <button className="flex align-center find-new-friends-add-btn">
-                    Add <PersonAddIcon style={{ paddingLeft: "5px" }} />
+                  <button
+                    className="flex align-center find-new-friends-add-btn"
+                    onClick={() => {
+                      if (friendRequestedUser?.includes(user._id)) {
+                        cancelFriendRequest(user._id)
+                      } else {
+                        sendFriendRequest(user._id)
+                      }
+                    }}
+                  >
+                    {friendRequestedUser.includes(user._id) ? "Cancel" : "Add"}
+                    <PersonAddIcon style={{ paddingLeft: "5px" }} />
                   </button>
                 </div>
               ))}
