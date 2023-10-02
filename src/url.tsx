8f1baef5c689ac4d00254ea30fd7761534e7bf18
Karan <rajputkaran441@gmail.com>
2023-10-03 00:29:04 +0530

    url changed to original server

--- src/url.tsx
+++ src/url.tsx
@@ -1,5 +1,5 @@
-// const URL = "https://virmigo-backend.vercel.app"
-const URL = "https://virmigo-backend-dummy.vercel.app"
+const URL = "https://virmigo-backend.vercel.app"
+// const URL = "https://virmigo-backend-dummy.vercel.app"
 // const URL = "http://localhost:3000"
 
 export default URL
